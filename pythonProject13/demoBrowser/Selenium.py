ad94c7b82db13b7334ddd4e3a612c2991c336447
Emanuel2592 <thomasep@mymail.vcu.edu>
2021-06-29 13:27:23 -0400

    SeleniumOnRahulShetty.com

--- pythonProject13/demoBrowser/Selenium.py
+++ pythonProject13/demoBrowser/Selenium.py
@@ -9,7 +9,7 @@ driver=webdriver.Chrome(executable_path="C:/Users/e_man/OneDrive/Desktop/python_
 driver.get("https://rahulshettyacademy.com/angularpractice")
 #get the website by name and send the keys
 
-driver.find_element_by_name("name").send_keys("Emanuel Thomas")
+driver.find_element_by_name("name").send_keys("Sameer Chandravanshi")
 
 #get the website ID and click on it
 driver.find_element_by_id("exampleCheck1").click()
@@ -23,18 +23,51 @@ driver.find_element_by_css_selector("Input[name='name']").send_keys("Emanuel")
 
 driver.find_element_by_css_selector("Input[class='form-control ng-untouched ng-pristine ng-invalid']").send_keys("lodu")
 
+#driver.find_element_by_css_selector("input[type='submit']").click()
 
+#========================================================================================================
+#Finding an element by utlizing all the classes from css selector
+#print(driver.find_element_by_css_selector("div[class='alert alert-success alert-dismissible']").text)
+#========================================================================================================
+
+################################VALIDATION ASSERTION########Finding an element using a class######################
+
+#Finding an element by utlizing a single class from css selector
+#print(driver.find_element_by_css_selector("div[class*='alert']").text)
+
+#print(driver.find_element_by_class_name("alert-success").text)
 
 ##Xpath is same css Selector
 driver.find_element_by_css_selector("Input[name='name']").send_keys("Emanuel")
 
+
+
+
+#========================================================================================================
 #X-Path
 driver.find_element_by_xpath("//Input[@name='name']").send_keys(" Thomas")
 
+#driver.find_element_by_xpath("//input[@type='submit']").click()
+
+#driver.find_element_by_xpath("//input[@type='submit']").click()
+
 driver.find_element_by_xpath("//input[@type='submit']").click()
 
+#FInding the by text by all the classes mentioned in it
+print(driver.find_element_by_xpath("//div[@class='alert alert-success alert-dismissible']").text)
+
+#======================================================================================
+#X-Path contains by any one class
+print(driver.find_element_by_xpath("//div[contains(@class,'alert-success')]").text)
+#//div[contains(@class,'alert')]
+#======================================================================================
+
+input#username
 
 
+print(driver.find_element_by_xpath("//div[contains(@class,'alert-success')]"))
+
+print(driver.find_element_by_xpath("//div[contains(@class,'alert')]"))
 
 print(driver.title)
 print(driver.current_url)
